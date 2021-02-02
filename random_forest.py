3b33f8e4780718a58578203d0cffff0b9fb55b73
Monwar Adeeb <monwar_adeeb@yahoo.com>
2021-02-02 10:22:14 +0600

    Worked on Random Forest

--- random_forest.py
+++ random_forest.py
@@ -2,3 +2,13 @@ import numpy
 import pandas
 import random
 from decisionTree import buildDecisionTree, decisionTreePredictions
+
+
+def trainTestSplit(dataFrame, testSize):
+    if isinstance(testSize, float):
+        testSize = round(testSize * len(dataFrame))
+    indices = dataFrame.index.tolist()
+    testIndices = random.sample(population=indices, k=testSize)
+    dataFrameTest = dataFrame.loc[testIndices]
+    dataFrameTrain = dataFrame.drop(testIndices)
+    return dataFrameTrain, dataFrameTest
