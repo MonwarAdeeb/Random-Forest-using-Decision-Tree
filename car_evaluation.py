e8cec8340b87d1cd2147fbcfffbe7aff59f86234
Monwar Adeeb <monwar_adeeb@yahoo.com>
2021-02-07 10:32:44 +0600

    Worked on Car Evaluation

--- car_evaluation.py
+++ car_evaluation.py
@@ -31,3 +31,23 @@ print("  Maximum random subspace size (d) is {}".format(
     dataFrameTrain.shape[1] - 1))
 
 print("\n  Change n, keep other parameters")
+
+for i in range(200, dataFrameTrain.shape[0] + 1, 100):
+    startTime = time.time()
+    randomForest = createRandomForest(
+        dataFrameTrain, bootstrapSize=i, randomAttributes=5, randomSplits=10, forestSize=10, treeMaxDepth=8)
+    buildingTime = time.time() - startTime
+    randomForestTestResults = randomForestPredictions(
+        dataFrameTest, randomForest)
+    accuracyTest = calculateAccuracy(
+        randomForestTestResults, dataFrameTest.iloc[:, -1]) * 100
+    randomForestTrainResults = randomForestPredictions(
+        dataFrameTrain, randomForest)
+    accuracyTrain = calculateAccuracy(
+        randomForestTrainResults, dataFrameTrain.iloc[:, -1]) * 100
+    print("  n = {}, d = {}, s = {}, k = {}, maxDepth = {}:".format(i, 5, 10, 10, 8))
+    print("    accTest = {0:.2f}%, ".format(accuracyTest), end="")
+    print("accTrain = {0:.2f}%, ".format(accuracyTrain), end="")
+    print("buildTime = {0:.2f}s".format(buildingTime), end="\n")
+
+print("\n  Change d, keep other parameters")
