f67a0d4ab31dc99a9aafcc88721a249ab13b1f29
Monwar Adeeb <monwar_adeeb@yahoo.com>
2021-02-08 10:38:44 +0600

    Worked on Car Evaluation

--- car_evaluation.py
+++ car_evaluation.py
@@ -51,3 +51,21 @@ for i in range(200, dataFrameTrain.shape[0] + 1, 100):
     print("buildTime = {0:.2f}s".format(buildingTime), end="\n")
 
 print("\n  Change d, keep other parameters")
+
+for i in range(1, dataFrameTrain.shape[1], 1):
+    startTime = time.time()
+    randomForest = createRandomForest(dataFrameTrain, bootstrapSize=200,
+                                      randomAttributes=i, randomSplits=10, forestSize=10, treeMaxDepth=8)
+    buildingTime = time.time() - startTime
+    randomForestTestResults = randomForestPredictions(
+        dataFrameTest, randomForest)
+    accuracyTest = calculateAccuracy(
+        randomForestTestResults, dataFrameTest.iloc[:, -1]) * 100
+    randomForestTrainResults = randomForestPredictions(
+        dataFrameTrain, randomForest)
+    accuracyTrain = calculateAccuracy(
+        randomForestTrainResults, dataFrameTrain.iloc[:, -1]) * 100
+    print("  n = {}, d = {}, s = {}, k = {}, maxDepth = {}:".format(200, i, 10, 10, 8))
+    print("    accTest = {0:.2f}%, ".format(accuracyTest), end="")
+    print("accTrain = {0:.2f}%, ".format(accuracyTrain), end="")
+    print("buildTime = {0:.2f}s".format(buildingTime), end="\n")
