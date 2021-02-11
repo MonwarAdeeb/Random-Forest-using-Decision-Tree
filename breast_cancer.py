d2d254186d93b38ea719a5addfcf188925896e3c
Monwar Adeeb <monwar_adeeb@yahoo.com>
2021-02-11 10:22:23 +0600

    Worked on Breast Cancer

--- breast_cancer.py
+++ breast_cancer.py
@@ -2,3 +2,9 @@ import random
 import pandas
 import time
 from random_forest import trainTestSplit, createRandomForest, randomForestPredictions, calculateAccuracy
+
+dataFrame = pandas.read_csv("dataset_files/breast_cancer.csv")
+dataFrame = dataFrame.drop("id", axis=1)
+dataFrame = dataFrame[dataFrame.columns.tolist()[1:] +
+                      dataFrame.columns.tolist()[0: 1]]
+dataFrameTrain, dataFrameTest = trainTestSplit(dataFrame, testSize=0.25)
