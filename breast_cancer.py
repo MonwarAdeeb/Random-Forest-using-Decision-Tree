83f8437691ef394d5641b0b7ce59e4c836d0b226
Monwar Adeeb <monwar_adeeb@yahoo.com>
2021-02-11 10:24:30 +0600

    Worked on Breast Cancer

--- breast_cancer.py
+++ breast_cancer.py
@@ -8,3 +8,8 @@ dataFrame = dataFrame.drop("id", axis=1)
 dataFrame = dataFrame[dataFrame.columns.tolist()[1:] +
                       dataFrame.columns.tolist()[0: 1]]
 dataFrameTrain, dataFrameTest = trainTestSplit(dataFrame, testSize=0.25)
+
+print("Random Forest - Breast Cancer Dataset")
+print("  Maximum bootstrap size (n) is {}".format(dataFrameTrain.shape[0]))
+print("  Maximum random subspace size (d) is {}".format(
+    dataFrameTrain.shape[1] - 1))
