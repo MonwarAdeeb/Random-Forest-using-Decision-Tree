d1cc94a30860e4c4231ecc6faff8692cee43eed2
Monwar Adeeb <monwar_adeeb@yahoo.com>
2021-02-13 10:03:06 +0600

    Finished Working on Breast Cancer

--- breast_cancer.py
+++ breast_cancer.py
@@ -72,3 +72,22 @@ for i in range(10, 100 + 1, 10):
     print("    accTest = {0:.2f}%, ".format(accuracyTest), end="")
     print("accTrain = {0:.2f}%, ".format(accuracyTrain), end="")
     print("buildTime = {0:.2f}s".format(buildingTime), end="\n")
+
+print("\n  Change k, keep other parameters")
+for i in range(10, 100 + 1, 10):
+    startTime = time.time()
+    randomForest = createRandomForest(dataFrameTrain, bootstrapSize=60,
+                                      randomAttributes=10, randomSplits=50, forestSize=i, treeMaxDepth=3)
+    buildingTime = time.time() - startTime
+    randomForestTestResults = randomForestPredictions(
+        dataFrameTest, randomForest)
+    accuracyTest = calculateAccuracy(
+        randomForestTestResults, dataFrameTest.iloc[:, -1]) * 100
+    randomForestTrainResults = randomForestPredictions(
+        dataFrameTrain, randomForest)
+    accuracyTrain = calculateAccuracy(
+        randomForestTrainResults, dataFrameTrain.iloc[:, -1]) * 100
+    print("  n = {}, d = {}, s = {}, k = {}, maxDepth = {}:".format(60, 10, 50, i, 3))
+    print("    accTest = {0:.2f}%, ".format(accuracyTest), end="")
+    print("accTrain = {0:.2f}%, ".format(accuracyTrain), end="")
+    print("buildTime = {0:.2f}s".format(buildingTime), end="\n")
